289837a4f608fdb42bd8c4a80985fad39019638d
AlexRErwin <aerwin1@csuchico.edu>
2025-08-05 11:44:19 -0700

    added DOM

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 window.onload = function() {
-  alert("script.js has loaded!");
+  //alert("script.js has loaded!");
+  const quote_collection = [ "first option" , "second option", "third option" ];
+  let random_num = Math.floor(Math.random() * 10);
+  document.getElementById("quote_space").innerHTML = quote_collection[random_num];
+
 };

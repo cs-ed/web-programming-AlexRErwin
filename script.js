39662ef0982584358c4f89d442a62481a6fe6441
AlexRErwin <aerwin1@csuchico.edu>
2025-08-05 14:50:42 -0700

    added click

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 window.onload = function() {
-  //alert("script.js has loaded!");
   const quote_collection = [ "first option" , "second option", "third option" ];
-  let random_num = Math.floor(Math.random() * 10);
+  let random_num = Math.floor(Math.random() * quote_collection.length);
+  /*
+  console.log(random_num);
+  console.log(quote_collection[random_num]);
+  */
   document.getElementById("quote_space").innerHTML = quote_collection[random_num];
 
 };
+
+document.getElementById("new_option").addEventListener("click", function()) {
+  console.log("Requesting new option...");
+}

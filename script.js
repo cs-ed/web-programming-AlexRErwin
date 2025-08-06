68fa477ade03b59b4d3ad72473b816957ec145df
AlexRErwin <aerwin1@csuchico.edu>
2025-08-05 21:53:50 -0700

    favicon

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const quote_collection = [ "first option" , "second option", "third option" ];
 
 function getNewOption() {
-  let random_num = Math.floor(Math.random() * quote_collection.length);
-  document.getElementById("quote_space").innerHTML = quote_collection[random_num];
+  let random_num
+  do{
+    random_num = Math.floor(Math.random() * quote_collection.length);
+  console.log("Generated random number: " + random_num + "which is quote " + quote_collection[random_num]);} 
+  while( quote_collection[random_num] == document.getElementById("quote_space").innerHTML );
+    document.getElementById("quote_space").innerHTML = quote_collection[random_num];
 };
 
 window.onload = function() {
